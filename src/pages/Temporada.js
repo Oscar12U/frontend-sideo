92335ab7ccde62cb6d4585444675bf7f1111b679
JuanKaVal <jcvalos18@gmail.com>
2021-06-01 22:00:39 -0600

    Temporada X FrontEnd Listo

--- src/pages/Temporada.js
+++ src/pages/Temporada.js
@@ -1,4 +1,4 @@
-import { Col, Row, Container } from "react-bootstrap";
+import { Button1, Col, Row, Container } from "react-bootstrap";
 import React from "react";
 import TopMenuBar from "../components/TopMenuBar";
 import { makeStyles } from "@material-ui/core/styles";
@@ -6,117 +6,114 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import AssessmentIcon from '@material-ui/icons/Assessment';
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
-import CreateIcon from '@material-ui/icons/Create';
-import PanToolIcon from '@material-ui/icons/PanTool';
-import Button from '@material-ui/core/Button';
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
+import CreateIcon from "@material-ui/icons/Create";
+import PanToolIcon from "@material-ui/icons/PanTool";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const Temporada = () => {
-
   const useStyles = makeStyles((theme) => ({
     root: {
-      width: '100%',
+      width: "100%",
       maxWidth: 360,
-      backgroundColor: '#118DD0',
-      borderRadius: '15px',
-      margin: 'auto',
+      backgroundColor: "#445074",
+      borderRadius: "15px",
+      margin: "auto",
+    },
+    btn: {
+      backgroundColor: "#445074",
+      color: "#FBFEF9",
+      height: "40px",
+      marginLeft: "8px",
+      // margin: "auto",
+    },
+    txt: {
+      color: "#FBFEF9",
+      font: "bold",
     },
   }));
 
-  const textStyles = makeStyles((theme) => ({
-    root: {
-      color: '#FBFEF9',
-      font: 'bold',
-    },
-  }));
-  
   const classes = useStyles();
-  const text = textStyles();
 
   return (
     <>
       <TopMenuBar></TopMenuBar>
       <br></br>
-      <Row>
-      <Container fluid="md">
-        <input md="auto" type="text" id="fname" name="fname"></input>
-        <Button variant="contained" margin>Guardar</Button>
-        <br></br>
-      </Container>
-      </Row>
       <br></br>
       <br></br>
-      <Container md="center">
-      
 
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Temporada"
+          variant="outlined"
+          size="small"
+        />
+        <Button className={classes.btn} variant="contained">
+          Guardar
+        </Button>
+      </Container>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <Container style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
         <List component="nav" className={classes.root} aria-label="contacts">
           <ListItem button>
-            <ListItemIcon className={text.root}>
+            <ListItemIcon className={classes.txt}>
               <CreateIcon />
             </ListItemIcon>
-            <ListItemText className={text.root} inset primary="Crear Partido" />
+            <ListItemText
+              className={classes.txt}
+              inset
+              primary="Crear Partido"
+            />
           </ListItem>
           <ListItem button>
-          <ListItemIcon className={text.root}>
+            <ListItemIcon className={classes.txt}>
               <AssessmentIcon />
             </ListItemIcon>
-            <ListItemText className={text.root} inset primary="Estadisticas de Temporada" />
+            <ListItemText
+              className={classes.txt}
+              inset
+              primary="Estadisticas de Temporada"
+            />
           </ListItem>
           <ListItem button>
-          <ListItemIcon className={text.root}>
+            <ListItemIcon className={classes.txt}>
               <DirectionsRunIcon />
             </ListItemIcon>
-            <ListItemText className={text.root} inset primary="Crear Entrenamiento" />
+            <ListItemText
+              className={classes.txt}
+              inset
+              primary="Crear Entrenamiento"
+            />
           </ListItem>
           <ListItem button>
-          <ListItemIcon className={text.root}>
+            <ListItemIcon className={classes.txt}>
               <PanToolIcon />
             </ListItemIcon>
-            <ListItemText className={text.root} inset primary="Gestionar Partido" />
+            <ListItemText
+              className={classes.txt}
+              inset
+              primary="Gestionar Partido"
+            />
           </ListItem>
         </List>
-
-
-        {/*   <Row>
-          <Col></Col>
-          <Col md="center">
-            <Button variant="primary ml-2" md="auto">
-              Crear Partido
-            </Button>
-          </Col>
-          <Col></Col>
-        </Row>
-        <br></br>
-        <Row>
-          <Col></Col>
-          <Col md="center">
-            <Button variant="primary ml-2" md="auto">
-              Estadisticas de Temporada
-            </Button>
-          </Col>
-          <Col></Col>
-        </Row>
-        <br></br>
-        <Row>
-          <Col></Col>
-          <Col md="center">
-            <Button variant="primary ml-2" md="auto">
-              Crear Entrenamiento
-            </Button>
-          </Col>
-          <Col></Col>
-        </Row>
-        <br></br>
-        <Row>
-          <Col></Col>
-          <Col md="center">
-            <Button variant="primary ml-2" md="auto">
-              Gestionar Partido
-            </Button>
-          </Col>
-          <Col></Col>
-        </Row> */}
       </Container>
     </>
   );
