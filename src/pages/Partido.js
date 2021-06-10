2932a1e4da715ceb2261239abe8c491c1f7c6ac3
Oscar Ulate <coca12799@gmail.com>
2021-06-09 21:54:25 -0600

    Pagina de Partido con todos los elementos y el cronometro

--- src/pages/Partido.js
+++ src/pages/Partido.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import Timer from "../components/Timer";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  DropdownButton,
+  Dropdown,
+} from "react-bootstrap";
 import TopMenuBar from "../components/TopMenuBar";
 import PartidoIcon from "@material-ui/icons/SportsSoccer";
 import TeamIcon from "@material-ui/icons/People";
@@ -77,27 +86,110 @@ export default function ScrollableTabsButtonForce() {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        <Container fluid="md" style={{ margin: "60px auto" }} elevation={3}>
+        <Container
+          fluid="md"
+          style={{ margin: "60px auto", backgroundcolor: "#fefbd8" }}
+          elevation={3}
+        >
           <Paper elevation={3}>
-            <Row style={{ margin: "auto 50px" }}>
+            <Row style={{ margin: "auto 20px" }}>
               <Col sm="8">
                 <h1>Detallles del Partido</h1>
                 <h3>Partido Contra: YYY</h3>
                 <h3>Temporada: X</h3>
               </Col>
-              <Col sm="4">
+              <Col sm="4" style={{ backgroundColor: "#787682" }}>
                 <h1>Tiempo del partido</h1>
-                <h3>Ha transcurrido: 000 Minutos</h3>
+                <Timer style={{ backgroundColor: "#787682" }} />
               </Col>
             </Row>
           </Paper>
-          <Row style={{ margin: "40px 50px" }}>
-            <br></br>
-            <br></br>
-            <br></br>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
+          <Row
+            style={{
+              margin: "40px 50px",
+              columnRuleStyle: "groove",
+              columnRuleWidth: "3px",
+              columnRuleColor: "lightcoral",
+            }}
+          >
+            <Col style={{ borderRight: "3px solid #A9A9A9" }}>
+              <h3>Seleccionar jugador</h3>
+              <DropdownButton
+                id="dropdown-item-button"
+                title="Seleccionar Jugador"
+              >
+                <Dropdown.ItemText>Lista Jugadores</Dropdown.ItemText>
+                <Dropdown.Item as="button">Jugador 1</Dropdown.Item>
+                <Dropdown.Item as="button">Jugador 2</Dropdown.Item>
+                <Dropdown.Item as="button">Jugador 3</Dropdown.Item>
+              </DropdownButton>
+            </Col>
+            <Col style={{ borderRight: "3px solid #A9A9A9" }}>
+              <Container fluid="md" style={{ margin: "auto" }}>
+                <Row>
+                  <Col>
+                    <Button variant="primary" size="lg">
+                      Gol
+                    </Button>{" "}
+                  </Col>
+                  <Col>
+                    <Form>
+                      {["A favor", "En contra"].map((type) => (
+                        <div key={`default-${type}`} className="mb-3">
+                          <Form.Check
+                            type={"checkbox"}
+                            id={`${type}`}
+                            label={`${type}`}
+                          />
+                        </div>
+                      ))}
+                    </Form>
+                  </Col>
+                </Row>
+                <Row style={{ margin: "20px auto" }}>
+                  <Button variant="secondary" size="lg">
+                    Falta
+                  </Button>
+                </Row>
+                <Row style={{ margin: "10px auto" }}>
+                  <Button variant="secondary" size="lg">
+                    Lesion
+                  </Button>
+                </Row>
+              </Container>
+            </Col>
+            <Col>
+              <Container>
+                <Col>
+                  <Row>
+                    <h3>Jugador Entra</h3>
+                    <br></br>
+                    <DropdownButton
+                      id="dropdown-item-button"
+                      title="Seleccionar Jugador"
+                    >
+                      <Dropdown.ItemText>Lista Jugadores</Dropdown.ItemText>
+                      <Dropdown.Item as="button">Jugador 1</Dropdown.Item>
+                      <Dropdown.Item as="button">Jugador 2</Dropdown.Item>
+                      <Dropdown.Item as="button">Jugador 3</Dropdown.Item>
+                    </DropdownButton>
+                  </Row>
+                  <Row>
+                    <h3>Jugador Sale</h3>
+                    <br></br>
+                    <DropdownButton
+                      id="dropdown-item-button"
+                      title="Seleccionar Jugador"
+                    >
+                      <Dropdown.ItemText>Lista Jugadores</Dropdown.ItemText>
+                      <Dropdown.Item as="button">Jugador 1</Dropdown.Item>
+                      <Dropdown.Item as="button">Jugador 2</Dropdown.Item>
+                      <Dropdown.Item as="button">Jugador 3</Dropdown.Item>
+                    </DropdownButton>
+                  </Row>
+                </Col>
+              </Container>
+            </Col>
           </Row>
         </Container>
       </TabPanel>
